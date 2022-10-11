b6c48465c365044314ec525bdea58b89f66b4d7d
Adilkhan Abdrashov <67748974+AdilkhanAbdrashov@users.noreply.github.com>
2022-10-11 09:48:45 +0600

    Security

---
+++ src/main/java/cefdemo/security/configuration/MySecurityInitializer.java
@@ -0,0 +1,6 @@
+package cefdemo.security.configuration;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class MySecurityInitializer extends AbstractSecurityWebApplicationInitializer {
+}
